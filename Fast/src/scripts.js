f5e4ee05b0082191a4b47fe5b342c7de2a88207e
ZhirDev <zheerjalal22@gmail.com>
2025-06-23 03:19:47 +0300

    assets/css update new

---
+++ Fast/src/scripts.js
@@ -0,0 +1,108 @@
+window.addEventListener('load',init);
+let time = 6;
+let score = 0;
+let isPlaying;
+
+//DOM 
+const currentWord = document.querySelector('#current-word');
+const wordInput = document.getElementById('word-input');
+const timeDisplay= document.querySelector('#time');
+const scoreDisplay= document.querySelector('#score');
+const message = document.querySelector('#message');
+
+
+// array
+
+const words = [
+    "ژیر",
+    "ڤێنوار",
+    "ولیام",
+    "خواستی",
+    "هەستی",
+    "پەیوەند",
+    "محمەد",
+    "ئاکار",
+    "حوسێن",
+    "ڕامین",
+    "ئەحمد",
+    "کۆڤین",
+    "شادە",
+    "مەنسا",
+    "خولەی بچووک",
+    "فلاوفل",
+    "مریشک",
+    "ئەنەس",
+    "کتێب",
+    "جگەر",
+    "کوبە",
+    "شفتە",
+    "بۆرەک",
+    "ماسی",
+    "کەر",
+    "بزن",
+    "مەڕ",
+    "کۆتر",
+    "شێر",
+    "کەروێشک"
+];
+
+
+// init game 
+
+function init(){
+    showWord(words);
+
+    setInterval(countDown,1000);
+
+    setInterval(cheackStatus,50);
+
+    wordInput.addEventListener('input',startGame);
+
+}
+
+function showWord(words){
+    const randIndex =Math.floor(Math.random()* words.length);
+   currentWord.innerHTML = words[randIndex];
+}
+
+function countDown(){
+    if (time>0){
+        time--;
+    }else if (time ==0){
+    isPlaying = false;
+    }
+    timeDisplay.innerHTML=time;
+}
+
+
+function cheackStatus(){
+    if (time === 0 && isPlaying === false){
+        message.innerHTML = 'game over';
+        score=-1;
+    }
+}
+
+function startGame(){
+    if(matchWord()){
+        isPlaying = true;
+        time = 6;
+        showWord(words);
+        wordInput.value ='';
+        score++;
+    }
+    if(score === -1){
+        scoreDisplay.innerHTML = 0;
+    }else{
+        scoreDisplay.innerHTML=score;
+    }
+}
+
+function matchWord(){
+    if(wordInput.value === currentWord.innerHTML){
+        message.innerHTML = 'correct';
+        return true;
+    }else {
+        message.innerHTML = '';
+        return false;
+    }
+}
